a3a5d62d00104d7ecff422a15e74fbbfb9c94414
Brian Batz <61260258+bedhsk@users.noreply.github.com>
2023-11-22 06:22:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['html', 'css', 'js', 'react'];
+    var member = {
+        name: 'Choi',
+        age: 27,
+        skills: skills
+    };
+    console.log(member.skills);
+}
